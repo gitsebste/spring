371a553b6b41eb76af074f096b04bb0c8ca2e915
device02 <seb_email@o2.pl>
2018-06-22 09:19:59 +0200

    small... part of GUI (table)

--- src/main/java/com/example/demo/controller/UnitController.java
+++ src/main/java/com/example/demo/controller/UnitController.java
@@ -25,8 +25,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/unit")
 public class UnitController {
-    
-    
+        
     UnitService service;
 
     public UnitService getService() {
@@ -39,80 +38,41 @@ public class UnitController {
         
     @RequestMapping(value="/add",method=RequestMethod.GET)
         public String add(HttpServletRequest httpServletRequest,Model model) {
-        
-        //logger.debug("ItemController.add()");
-        //System.out.println("com.example.demo.controller.homecontroller.add()");
-        
-        //String msg = httpServletRequest.getParameter("msg");
-        //if(msg==null)msg="Hello world!";
-        
-        //model.addAttribute("msg",msg);
-        
-        
-        
-        return  "addUnit";        
+            return  "addUnit";        
     }
             @RequestMapping(value="/add",method=RequestMethod.POST)
         public String added(HttpServletRequest httpServletRequest,Model model) {
-        
-        //logger.debug("ItemController.add()");
-        //System.out.println("com.example.demo.controller.homecontroller.add()");
-        
-        String name = httpServletRequest.getParameter("name");
-        String shortName = httpServletRequest.getParameter("shortName");
-        
-        
-        
-        //sending object to model&view
-        //Item i = new Item();i.setCode("codeOfTheItem");
-        //model.addAttribute("obj",i);
-        //<h1 th:text="${obj.code}"></h1><br>
-        
-        //validation in code and in html
-        
-        
-        
-                //System.out.println("Successfully validated. Adding to database...");
-                //System.out.println(new Unit(name, shortName));
-                service.save(new Unit(name, shortName));
-        
-        return  "home";        
-    }
-        
+            String name = httpServletRequest.getParameter("name");
+            String shortName = httpServletRequest.getParameter("shortName");
+            service.save(new Unit(name, shortName));        
+            return  "home";        
+    }        
             @RequestMapping(value="/showAll",method=RequestMethod.GET)
         public String showAll(HttpServletRequest httpServletRequest,Model model) {  
             model.addAttribute("iterable", service.getAll());
         return  "showUnits";        
     }
-        
-                    @RequestMapping(value="/byShortName",method=RequestMethod.GET)
+        @RequestMapping(value="/byShortName",method=RequestMethod.GET)
         public String showByShortName(HttpServletRequest httpServletRequest,Model model) {
             String shortName = httpServletRequest.getParameter("shortName");
             model.addAttribute("iterable", service.getByShortName(shortName));
         return  "showUnits";        
     }
-                            @RequestMapping(value="/chosenByUser/{id}",method=RequestMethod.GET)
+        @RequestMapping(value="/chosenByUser/{id}",method=RequestMethod.GET)
         public String showById(HttpServletRequest httpServletRequest,Model model,@PathVariable("id") int id) {
             model.addAttribute("el", service.getById(id));
-            //model.addAttribute("unit", new Unit());
         return  "showUnit";        
     }
-        
-                                    @RequestMapping(value="/delete/{id}",method=RequestMethod.GET)
+        @RequestMapping(value="/delete/{id}",method=RequestMethod.GET)
         public String deleteById(HttpServletRequest httpServletRequest,Model model,@PathVariable("id") int id) {
-            //model.addAttribute("el", service.getById(id));
             service.deleteById(id);
         return  "home";        
     }
-        
-                                    @RequestMapping(value="/save",method=RequestMethod.GET)
-        public String save(HttpServletRequest httpServletRequest,Model model ) {            
-            
+        @RequestMapping(value="/save",method=RequestMethod.GET)
+        public String save(HttpServletRequest httpServletRequest,Model model ) {                        
             String id = httpServletRequest.getParameter("id");
             String name = httpServletRequest.getParameter("name");
-            String shortName = httpServletRequest.getParameter("shortName");
-            
-            //System.out.println("'Saving' "+id+" "+name+" "+shortName);
+            String shortName = httpServletRequest.getParameter("shortName");           
             Unit unit=new Unit(name, shortName);unit.setId(Integer.valueOf(id));
             service.save(unit);
             
